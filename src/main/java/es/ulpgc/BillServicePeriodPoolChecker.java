0a58f8f651cb2498c0c343154fbaf7f57ae8fd34
RaulLozanoPonce <raul.lozano.ponce@hotmail.com>
2020-01-21 19:22:20 +0000

    BillServicePeriodPoolChecker addBill tests considering overlapping and if having gaps have been passed

--- src/main/java/es/ulpgc/BillServicePeriodPoolChecker.java
+++ src/main/java/es/ulpgc/BillServicePeriodPoolChecker.java
@@ -1,5 +1,6 @@
 package es.ulpgc;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class BillServicePeriodPoolChecker {
@@ -16,33 +17,89 @@ public class BillServicePeriodPoolChecker {
         Date dateNow = updaterDate.now();
         billServicePeriodPool.setFinishDate(dateNow);
         if(billServicePeriod.getFinishDate().compareTo(dateNow) <= 0) {
-            if (billServicePeriodPool.getStartDate() == null) {
-                if (billServicePeriodPool.getBillTimeLine().add(billServicePeriod)) {
-                    billServicePeriodPool.setStartDate(billServicePeriod.getStartDate());
-                    return true;
+            List<BillServicePeriod> billServicePeriodList = new ArrayList<>(billServicePeriodPool.getBillTimeLine());
+            if(!isOverlapping(billServicePeriodList, billServicePeriod) && !isHavingGaps(billServicePeriodList, billServicePeriod)) {
+                if (billServicePeriodPool.getStartDate() == null) {
+                    if (billServicePeriodPool.getBillTimeLine().add(billServicePeriod)) {
+                        billServicePeriodPool.setStartDate(billServicePeriod.getStartDate());
+                        return true;
+                    }
+                } else if (billServicePeriod.getFinishDate().compareTo(billServicePeriodPool.getStartDate()) >= 0) {
+                    return billServicePeriodPool.getBillTimeLine().add(billServicePeriod);
                 }
-            } else if (billServicePeriod.getFinishDate().compareTo(billServicePeriodPool.getStartDate()) >= 0) {
-                return billServicePeriodPool.getBillTimeLine().add(billServicePeriod);
             }
         }
         return false;
     }
 
     public boolean isOverlapping(List<BillServicePeriod> billServicePeriodList, BillServicePeriod billServicePeriod) {
-        if(billServicePeriod.getStartDate().compareTo(billServicePeriodList.get(0).getStartDate()) == 0) return true;
-        if(billServicePeriod.getFinishDate().compareTo(billServicePeriodList.get(billServicePeriodList.size() - 1).getFinishDate()) == 0) return true;
-        if(billServicePeriod.getStartDate().compareTo(billServicePeriodList.get(0).getStartDate()) < 0 &&
-                billServicePeriod.getFinishDate().compareTo(billServicePeriodList.get(0).getStartDate()) >= 0) return true;
-        if(billServicePeriod.getStartDate().compareTo(billServicePeriodList.get(0).getStartDate()) >= 0 &&
-                billServicePeriod.getFinishDate().compareTo(billServicePeriodList.get(billServicePeriodList.size() - 1).getFinishDate()) <= 0) return true;
-        if(billServicePeriod.getStartDate().compareTo(billServicePeriodList.get(0).getFinishDate()) <= 0 &&
-                billServicePeriod.getFinishDate().compareTo(billServicePeriodList.get(billServicePeriodList.size() - 1).getFinishDate()) > 0) return true;
+        if(billServicePeriodList.size() > 0) {
+            Date startDateList = billServicePeriodList.get(0).getStartDate();
+            Date finishDateList = billServicePeriodList.get(billServicePeriodList.size() - 1).getFinishDate();
+            Date startDateNew = billServicePeriod.getStartDate();
+            Date finishDateNew = billServicePeriod.getFinishDate();
+
+            if((startDateNew.compareTo(startDateList) < 0 && finishDateNew.compareTo(startDateList) < 0) ||
+                    (startDateNew.compareTo(finishDateList) > 0 && finishDateNew.compareTo(finishDateList) > 0)) {
+                return false;
+            }
+
+            if((startDateNew.compareTo(finishDateList) < 0 && finishDateNew.compareTo(finishDateList) == 0) ||
+                    (startDateNew.compareTo(startDateList) == 0 && finishDateNew.compareTo(finishDateList) == 0) ||
+                    (startDateNew.compareTo(startDateList) == 0 && finishDateNew.compareTo(finishDateList) > 0)) {
+                return true;
+            }
+
+            if((startDateNew.compareTo(startDateList) < 0 && finishDateNew.compareTo(startDateList) == 0) ||
+                    (startDateNew.compareTo(finishDateList) == 0 && finishDateNew.compareTo(finishDateList) > 0)) {
+                return true;
+            }
+
+            if(startDateNew.compareTo(startDateList) > 0 && finishDateNew.compareTo(startDateList) > 0 &&
+                    startDateNew.compareTo(finishDateList) < 0 && finishDateNew.compareTo(finishDateList) < 0) {
+                return true;
+            }
+
+            if(startDateNew.compareTo(startDateList) < 0 && finishDateNew.compareTo(startDateList) > 0 && finishDateNew.compareTo(finishDateList) < 0) {
+                return true;
+            }
+
+            if(startDateNew.compareTo(startDateList) > 0 && startDateNew.compareTo(finishDateList) < 0 && finishDateNew.compareTo(finishDateList) > 0) {
+                return true;
+            }
+
+            if(startDateNew.compareTo(startDateList) < 0 && finishDateNew.compareTo(finishDateList) > 0) {
+                return true;
+            }
+        }
         return false;
     }
 
+    /*
+    * if (billServicePeriod.getStartDate().compareTo(billServicePeriodList.get(0).getStartDate()) == 0)
+                return true;
+            if (billServicePeriod.getFinishDate().compareTo(billServicePeriodList.get(billServicePeriodList.size() - 1).getFinishDate()) == 0)
+                return true;
+            if (billServicePeriod.getStartDate().compareTo(billServicePeriodList.get(0).getStartDate()) < 0 &&
+                    billServicePeriod.getFinishDate().compareTo(billServicePeriodList.get(0).getStartDate()) >= 0)
+                return true;
+            if (billServicePeriod.getStartDate().compareTo(billServicePeriodList.get(billServicePeriodList.size() - 1).getFinishDate()) <= 0 &&
+                    billServicePeriod.getFinishDate().compareTo(billServicePeriodList.get(billServicePeriodList.size() - 1).getFinishDate()) > 0)
+                return true;
+            if (billServicePeriod.getStartDate().compareTo(billServicePeriodList.get(0).getStartDate()) >= 0 &&
+                    billServicePeriod.getFinishDate().compareTo(billServicePeriodList.get(billServicePeriodList.size() - 1).getFinishDate()) <= 0)
+                return true;
+            if (billServicePeriod.getStartDate().compareTo(billServicePeriodList.get(0).getFinishDate()) <= 0 &&
+                    billServicePeriod.getFinishDate().compareTo(billServicePeriodList.get(billServicePeriodList.size() - 1).getFinishDate()) > 0)
+                return true;
+    * */
+
     public boolean isHavingGaps(List<BillServicePeriod> billServicePeriodList, BillServicePeriod billServicePeriod) {
-        return billServicePeriod.getFinishDate().adjacentDays(billServicePeriodList.get(0).getStartDate()) == -2 ||
-                billServicePeriod.getStartDate().adjacentDays(billServicePeriodList.get(billServicePeriodList.size() - 1).getFinishDate()) == 2;
+        if(billServicePeriodList.size() > 0) {
+            return billServicePeriod.getFinishDate().adjacentDays(billServicePeriodList.get(0).getStartDate()) == -2 ||
+                    billServicePeriod.getStartDate().adjacentDays(billServicePeriodList.get(billServicePeriodList.size() - 1).getFinishDate()) == 2;
+        }
+        return false;
     }
 
     public Date getUpdaterDate() {
--- src/test/java/es/ulpgc/BillServicePeriodPoolCheckerAddBill_.java
+++ src/test/java/es/ulpgc/BillServicePeriodPoolCheckerAddBill_.java
@@ -15,6 +15,7 @@ public class BillServicePeriodPoolCheckerAddBill_ {
 
     static {
         Date dateNow = new Date(18, Date.APRIL, 2019);
+        Date dateNow2 = new Date(3, Date.JUNE, 2019);
 
         Date d1 = new Date(1, Date.FEBRUARY, 2019);
         Date d2 = new Date(3, Date.MARCH, 2019);
@@ -24,11 +25,21 @@ public class BillServicePeriodPoolCheckerAddBill_ {
         Date d6 = new Date(4, Date.MAY, 2019);
         Date d7 = new Date(5, Date.MAY, 2019);
         Date d8 = new Date(2, Date.JUNE, 2019);
+        Date d9 = new Date(2, Date.MARCH, 2019);
+        Date d10 = new Date(8, Date.MAY, 2019);
+        Date d11 = new Date(18, Date.MARCH, 2019);
+        Date d12 = new Date(18, Date.APRIL, 2019);
 
         BillServicePeriod bsp1 = new BillServicePeriod(d1, d2);
         BillServicePeriod bsp2 = new BillServicePeriod(d3, d4);
         BillServicePeriod bsp3 = new BillServicePeriod(d5, d6);
         BillServicePeriod bsp4 = new BillServicePeriod(d7, d8);
+
+        BillServicePeriod bsp5 = new BillServicePeriod(d1, d9);
+        BillServicePeriod bsp6 = new BillServicePeriod(d10, d8);
+        BillServicePeriod bsp7 = new BillServicePeriod(d1, d3);
+        BillServicePeriod bsp8 = new BillServicePeriod(d6, d8);
+        BillServicePeriod bsp9 = new BillServicePeriod(d11, d12);
 
         BillServicePeriodPool bspp1 = new BillServicePeriodPool();
         BillServicePeriodPool bspp2 = new BillServicePeriodPool(bsp1);
@@ -38,6 +49,10 @@ public class BillServicePeriodPoolCheckerAddBill_ {
         BillServicePeriodPool bspp6 = new BillServicePeriodPool(bsp1);
         BillServicePeriodPool bspp7 = new BillServicePeriodPool(d2, d3);
         BillServicePeriodPool bspp8 = new BillServicePeriodPool(bsp1, d1, d3);
+        BillServicePeriodPool bspp9 = new BillServicePeriodPool(d1, d8);
+        BillServicePeriodPool bspp10 = new BillServicePeriodPool(d1, d8);
+        BillServicePeriodPool bspp11 = new BillServicePeriodPool(d1, d8);
+        BillServicePeriodPool bspp12 = new BillServicePeriodPool(d1, d8);
 
         BillServicePeriodPoolChecker bsppc1 = new BillServicePeriodPoolChecker(bspp1);
         BillServicePeriodPoolChecker bsppc2 = new BillServicePeriodPoolChecker(bspp2);
@@ -47,6 +62,10 @@ public class BillServicePeriodPoolCheckerAddBill_ {
         BillServicePeriodPoolChecker bsppc6 = new BillServicePeriodPoolChecker(bspp6);
         BillServicePeriodPoolChecker bsppc7 = new BillServicePeriodPoolChecker(bspp7);
         BillServicePeriodPoolChecker bsppc8 = new BillServicePeriodPoolChecker(bspp8);
+        BillServicePeriodPoolChecker bsppc9 = new BillServicePeriodPoolChecker(bspp9);
+        BillServicePeriodPoolChecker bsppc10 = new BillServicePeriodPoolChecker(bspp10);
+        BillServicePeriodPoolChecker bsppc11 = new BillServicePeriodPoolChecker(bspp11);
+        BillServicePeriodPoolChecker bsppc12 = new BillServicePeriodPoolChecker(bspp12);
 
         bsppc1.setUpdaterDate(mock(Date.class));
         when(bsppc1.getUpdaterDate().now()).thenReturn(new Date(18, Date.APRIL, 2019));
@@ -56,7 +75,6 @@ public class BillServicePeriodPoolCheckerAddBill_ {
         when(bsppc3.getUpdaterDate().now()).thenReturn(new Date(18, Date.APRIL, 2019));
         bsppc4.setUpdaterDate(mock(Date.class));
         when(bsppc4.getUpdaterDate().now()).thenReturn(new Date(18, Date.APRIL, 2019));
-
         bsppc5.setUpdaterDate(mock(Date.class));
         when(bsppc5.getUpdaterDate().now()).thenReturn(new Date(18, Date.APRIL, 2019));
         bsppc6.setUpdaterDate(mock(Date.class));
@@ -65,17 +83,28 @@ public class BillServicePeriodPoolCheckerAddBill_ {
         when(bsppc7.getUpdaterDate().now()).thenReturn(new Date(18, Date.APRIL, 2019));
         bsppc8.setUpdaterDate(mock(Date.class));
         when(bsppc8.getUpdaterDate().now()).thenReturn(new Date(18, Date.APRIL, 2019));
+        bsppc9.setUpdaterDate(mock(Date.class));
+        when(bsppc9.getUpdaterDate().now()).thenReturn(new Date(3, Date.JUNE, 2019));
+        bsppc10.setUpdaterDate(mock(Date.class));
+        when(bsppc10.getUpdaterDate().now()).thenReturn(new Date(3, Date.JUNE, 2019));
+        bsppc11.setUpdaterDate(mock(Date.class));
+        when(bsppc11.getUpdaterDate().now()).thenReturn(new Date(3, Date.JUNE, 2019));
+        bsppc12.setUpdaterDate(mock(Date.class));
+        when(bsppc12.getUpdaterDate().now()).thenReturn(new Date(3, Date.JUNE, 2019));
 
         cases = new Object[][] {
                 {bsppc1, bspp1, new BillServicePeriod[]{bsp1}, 1, d1, dateNow, new boolean[] {true}},
                 {bsppc2, bspp2, new BillServicePeriod[]{bsp2}, 2, d1, dateNow, new boolean[] {true}},
                 {bsppc3, bspp3, new BillServicePeriod[]{bsp2}, 1, d2, dateNow, new boolean[] {true}},
                 {bsppc4, bspp4, new BillServicePeriod[]{bsp2}, 2, d1, dateNow, new boolean[] {true}},
-
                 {bsppc5, bspp5, new BillServicePeriod[]{bsp2, bsp1}, 1, d3, dateNow, new boolean[] {true, false}},
                 {bsppc6, bspp6, new BillServicePeriod[]{bsp2, bsp3}, 2, d1, dateNow, new boolean[] {true, false}},
                 {bsppc7, bspp7, new BillServicePeriod[]{bsp2, bsp1}, 2, d2, dateNow, new boolean[] {true, true}},
-                {bsppc8, bspp8, new BillServicePeriod[]{bsp2, bsp4}, 2, d1, dateNow, new boolean[] {true, false}}
+                {bsppc8, bspp8, new BillServicePeriod[]{bsp2, bsp4}, 2, d1, dateNow, new boolean[] {true, false}},
+                {bsppc9, bspp9, new BillServicePeriod[]{bsp2, bsp3, bsp1, bsp4}, 4, d1, dateNow2, new boolean[] {true, true, true, true}},
+                {bsppc10, bspp10, new BillServicePeriod[]{bsp2, bsp3, bsp5, bsp6}, 2, d1, dateNow2, new boolean[] {true, true, false, false}},
+                {bsppc11, bspp11, new BillServicePeriod[]{bsp2, bsp3, bsp7, bsp8}, 2, d1, dateNow2, new boolean[] {true, true, false, false}},
+                {bsppc12, bspp12, new BillServicePeriod[]{bsp2, bsp3, bsp2, bsp9}, 2, d1, dateNow2, new boolean[] {true, true, false, false}}
         };
     }
 

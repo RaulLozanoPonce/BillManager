aa8fa60fffbc357c8cb2a7ade3171f584dba4bd8
RaulLozanoPonce <raul.lozano.ponce@hotmail.com>
2020-01-21 20:29:08 +0000

    Second round of tests of Date

--- src/test/java/es/ulpgc/DateComparasion_.java
+++ src/test/java/es/ulpgc/DateComparasion_.java
@@ -12,13 +12,28 @@ public class DateComparasion_ {
     private static Object[][] cases;
 
     static {
-        Date d1 = new Date(1, Date.FEBRUARY, 2019);
+        Date d1 = new Date(1, Date.MARCH, 2019);
         Date d2 = new Date(3, Date.MARCH, 2019);
+        Date d3 = new Date(1, Date.FEBRUARY, 2019);
+        Date d4 = new Date(1, Date.MARCH, 2020);
+        Date d5 = new Date(7, Date.AUGUST, 2004);
 
         cases = new Object[][] {
+                {d1, d1, 0, 0},
                 {d1, d2, -1, -2},
                 {d2, d1, 1, 2},
-                {d1, d1, 0, 0}
+                {d1, d3, 1, 2},
+                {d3, d1, -1, -2},
+                {d1, d4, -1, -2},
+                {d4, d1, 1, 2},
+                {d2, d3, 1, 2},
+                {d3, d2, -1, -2},
+                {d2, d4, -1, -2},
+                {d4, d2, 1, 2},
+                {d3, d4, -1, -2},
+                {d4, d3, 1, 2},
+                {d1, d5, 1, 2},
+                {d5, d1, -1, -2}
         };
     }
 
